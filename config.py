0b677bfb2d3f5eba387628de817d4907778487c4
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2022-12-17 17:57:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = '5816246888:AAFlxo_PL-2Ea-rwlYRz5Nb8mWtyr64EgbM'
     APP_ID = '5080899'
     API_HASH = '6fc7f813cf96e6692990b752b43fd9c7'
-
+    USERS = '
     #comma seperated user id of users who are allowed to use
     ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1098504493').split(',')]
 
